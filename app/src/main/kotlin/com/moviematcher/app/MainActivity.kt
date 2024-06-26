49fcc847ce2de9414b51643ff13d891346a20de1
zouhir <rajdaouizouhir.pro@gmail.com>
2024-06-26 19:18:24 +0200

    feat: Create MovieMatcherApp.

--- app/src/main/kotlin/com/moviematcher/app/MainActivity.kt
+++ app/src/main/kotlin/com/moviematcher/app/MainActivity.kt
@@ -5,27 +5,45 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.moviematcher.authentication.login.presentation.LoginRoute
 import com.moviematcher.designsystem.theme.MovieMatcherTheme
+import org.koin.core.context.loadKoinModules
+import org.koin.dsl.module
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         enableEdgeToEdge()
+        val activityModule = module {
+            single<GoogleSignInClient> {
+                val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestIdToken(getString(R.string.default_web_client_id))
+                    .requestId()
+                    .build()
+                GoogleSignIn.getClient(this@MainActivity, gso)
+            }
+        }
+
+        loadKoinModules(activityModule)
         setContent {
             MovieMatcherTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .navigationBarsPadding()
+                ) { innerPadding ->
+                    MovieMatcherApp(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -33,17 +51,8 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MovieMatcherTheme {
-        Greeting("Android")
+fun MovieMatcherApp(modifier: Modifier = Modifier) {
+    Surface {
+        LoginRoute() {}
     }
 }
