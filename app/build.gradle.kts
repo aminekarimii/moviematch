7f476f6099dfb4a7ff2dca691ed09bb56acac343
zouhir <rajdaouizouhir.pro@gmail.com>
2024-07-03 22:49:44 +0200

    feat: Created session module.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,6 +24,7 @@ android {
 dependencies {
     implementation(project(":core:designsystem"))
     implementation(project(":feature:authentication"))
+    implementation(project(":feature:session"))
 
     // Google auth
     implementation(libs.play.services.auth)
