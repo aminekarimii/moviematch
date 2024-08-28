671e8a3ca0895b783c81cc47a7c1178ae23792c0
Amine Karimi <aminekarimi1998@gmail.com>
2024-08-28 18:35:29 +0100

    rename ProfileCardView.kt -> MatchMovieCardView.kt

--- feature/matching/src/main/kotlin/com/moviematcher/matching/presentation/match/MatchMovieCardView.kt
+++ feature/matching/src/main/kotlin/com/moviematcher/matching/presentation/match/MatchMovieCardView.kt
@@ -14,26 +14,17 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import com.moviematcher.designsystem.theme.MovieMatcherTheme
+import com.moviematcher.matching.presentation.matched_list.MovieModel
 
-class Item(
-    val id: String,
-    val name: String,
-    val age: Int,
-    val picture: String
-)
-
-val dummy = Item(
-    id = "1",
-    name = "John Doe",
-    age = 25,
-    picture = "https://picsum.photos/200/300"
-)
-val dummy1 = Item(
-    id = "2",
-    name = "Hello from",
-    age = 19,
-    picture = "https://fastly.picsum.photos/id/923/200/300.jpg?hmac=eiYSYaG7v46VlrE38Amrg33bd2FzVjaCsQrLMdekyAU"
+private val demoMovie = MovieModel(
+    index = 1,
+    title = "Money Heist - La casa de papel 2017 from Netflix",
+    year = "2021",
+    length = "2h 30m",
+    posterUrl = "https://image.tmdb.org/t/p/w500/6MKr3KgOLmzOP6MSuZERO41Lpkt.jpg"
 )
 
 @Preview(showSystemUi = true, showBackground = true)
@@ -41,7 +32,7 @@ val dummy1 = Item(
 fun PreviewDraggableCard() {
     MovieMatcherTheme {
         DraggableCardView(
-            profile = dummy,
+            movie = demoMovie,
             modifier = Modifier.aspectRatio(.6f)
         )
     }
@@ -50,7 +41,7 @@ fun PreviewDraggableCard() {
 @Composable
 fun DraggableCardView(
     modifier: Modifier = Modifier,
-    profile: Item,
+    movie: MovieModel,
 ) {
     Card(
         colors = CardDefaults.cardColors(containerColor = Color.Unspecified),
