e0ff11e70b01ddf16fe09a8d8b1b63e01e5d17fd
Amine Karimi <aminekarimi1998@gmail.com>
2024-09-30 17:08:53 +0100

    log ktor network requests

--- core/data/src/main/kotlin/com/moviematcher/data/di/NetworkModule.kt
+++ core/data/src/main/kotlin/com/moviematcher/data/di/NetworkModule.kt
@@ -1,5 +1,6 @@
 package com.moviematcher.data.di
 
+import android.util.Log
 import com.moviematcher.data.BuildConfig
 import com.moviematcher.data.network.interceptor.ApiKeyInterceptor
 import io.ktor.client.HttpClient
@@ -7,11 +8,14 @@ import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import org.koin.dsl.module
 
-const val BASE_URL = "https://api.themoviedb.org/3/"
+private const val BASE_URL = "https://api.themoviedb.org/3/"
 
 val jsonModule = module {
     single {
@@ -36,6 +40,15 @@ val okhttpModule = module {
 
     single<HttpClient> {
         HttpClient(get()) {
+            install(Logging) {this.
+                logger = object : Logger {
+                    override fun log(message: String) {
+                        Log.d("NetworkTag", message)
+                    }
+                }
+
+                level = LogLevel.ALL
+            }
             install(ContentNegotiation) {
                 json(get())
             }
