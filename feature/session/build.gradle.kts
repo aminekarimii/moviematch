7fa3c24a5da07d85b3617f04108298f6a45d2e4e
apachi1444 <yessinejawa@gmail.com>
2025-01-13 10:36:53 +0100

    Add new dependencies of zxing and gson

--- feature/session/build.gradle.kts
+++ feature/session/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("moviematcher.android.feature")
     id("moviematcher.android.library.compose")
+    id("kotlin-parcelize")
 }
 
 android {
@@ -11,4 +12,7 @@ dependencies {
     implementation(libs.io.coil)
     implementation(libs.firebase.auth)
 
+    implementation(libs.zxing.android.embedded)
+    implementation(libs.core)
+    implementation (libs.gson)
 }
