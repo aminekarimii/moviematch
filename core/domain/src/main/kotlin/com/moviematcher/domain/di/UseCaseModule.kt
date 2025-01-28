dd9e289197e4fd30c9ffdff75385c222e0415ffa
apachi1444 <yessinejawa@gmail.com>
2025-01-28 10:45:43 +0100

    Update modules for use case and presentation

--- core/domain/src/main/kotlin/com/moviematcher/domain/di/UseCaseModule.kt
+++ core/domain/src/main/kotlin/com/moviematcher/domain/di/UseCaseModule.kt
@@ -1,11 +1,13 @@
 package com.moviematcher.domain.di
 
 import com.moviematcher.domain.usecase.GetMatchedMoviesUseCase
+import com.moviematcher.domain.usecase.JoinSessionUseCase
 import com.moviematcher.domain.usecase.LoadMoviesBatchUseCase
 import kotlinx.coroutines.Dispatchers
 import org.koin.dsl.module
 
 val useCaseModule = module {
     factory { GetMatchedMoviesUseCase(get(), get(), Dispatchers.IO) }
+    factory { JoinSessionUseCase(get(), get(), Dispatchers.IO) }
     factory { LoadMoviesBatchUseCase(get(), Dispatchers.IO) }
 }
